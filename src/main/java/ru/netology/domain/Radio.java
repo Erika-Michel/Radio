2645bd568d287d41674ed5ea9ddefd8394c15a4d
Erika-Michel <erikamichel174@gmail.com>
2021-06-21 16:51:54 +0200

    Changed lombok annotations in Radio.java

--- src/main/java/ru/netology/domain/Radio.java
+++ src/main/java/ru/netology/domain/Radio.java
@@ -1,9 +1,12 @@
 package ru.netology.domain;
 
 import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
-@Data
+@Getter
+@Setter
 @NoArgsConstructor
 
 public class Radio {
